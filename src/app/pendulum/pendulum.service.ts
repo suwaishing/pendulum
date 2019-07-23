0d4ee3735c6f1444cff5878ebafdce62bf17c872
winston96 <w.shing96@gmail.com>
2019-07-23 13:41:39 +0700

    pen v0.2

--- src/app/pendulum/pendulum.service.ts
+++ src/app/pendulum/pendulum.service.ts
@@ -1,30 +1,35 @@
 import * as THREE from "three";
 import * as CANNON from "cannon";
-import GLTFLoader from 'three-gltf-loader';
-import {OrbitControls} from 'three-orbitcontrols-ts';
+import { OrbitControls } from 'three-orbitcontrols-ts';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
-export class PendulumService {
 
-  constructor() { }
+export class PendulumService {
   private world: CANNON.World;
   private canvas: HTMLCanvasElement;
   private renderer: THREE.WebGLRenderer;
   private camera: THREE.PerspectiveCamera;
   private scene: THREE.Scene;
-  private light: THREE.AmbientLight;
+  private controls: OrbitControls;
   private dragControl: DragControls;
-  private control: OrbitControls;
+  private mouse: THREE.Vector2;
+  private raycaster: THREE.Raycaster;
   private dt = 1 / 60;
+  //object
 
-  //sync
   private bodies: any[] = [];
   private meshes: any[] = [];
+  private mesh: THREE.Mesh;
+  private physicMaterial: CANNON.Material;
+  private groundMaterial: CANNON.Material;
+  private isDrag= false;
+  constructor() { }
 
-  createScene(id: string){
+  createScene(id: string) {
+
     this.canvas = <HTMLCanvasElement>document.getElementById(id);
 
     this.renderer = new THREE.WebGLRenderer({
@@ -36,24 +41,41 @@ export class PendulumService {
     this.renderer.shadowMap.enabled = true;
     this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
+    //this.renderer.setClearColor("rgb(98, 131, 149)");
     //Create scene
     this.scene = new THREE.Scene();
+    //this.scene.background= new THREE.Color( "rgb(98, 131, 149)" );
     this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, .1, 1000);
+    this.camera.position.set(8,2,10);
     this.scene.add(this.camera);
-    this.light = new THREE.AmbientLight( 0xf5bae3,1);
-    this.scene.add(this.light);
 
-    //this.control = new OrbitControls(this.camera, this.canvas);
+    
+    
+
+    //Create Light
+    let light = new THREE.AmbientLight(0xf8bce5);
+    //let light = new THREE.AmbientLight( 0xf5bae3,2);
+    //light.position.set(0,7,5);
+    this.scene.add(light);
+
+
+    //mouse and raycaster
+    this.mouse=new THREE.Vector2();
+    this.raycaster = new THREE.Raycaster();
 
     this.initCannon();
     //Create Floor
     this.createGround();
 
     //Load model
-    this.createChain();
+    this.addSphereChain();
+
+    this.controls = new OrbitControls(this.camera,this.renderer.domElement);
+    this.dragControl = new DragControls(this.meshes, this.camera, this.renderer.domElement);
+    console.log(this.bodies)
   }
 
-  animate(){
+  animate() {
     window.addEventListener('DOMContentLoaded', () => {
       this.render();
     })
@@ -61,7 +83,18 @@ export class PendulumService {
     window.addEventListener('resize', () => {
       this.resize();
     })
+    this.dragControl.addEventListener('dragstart', () =>{
+      this.controls.enabled = false;
+      this.isDrag=true;
+
+    });
+    this.dragControl.addEventListener('dragend', () =>{
+      this.controls.enabled = true;
+      this.isDrag=false;
+      
+    });
   }
+
 
   render() {
     requestAnimationFrame(() => {
@@ -69,13 +102,23 @@ export class PendulumService {
 
     });
     this.world.step(this.dt);
-    //this.control.update();
+    this.controls.update();
     // Update positions
 
+    if(this.isDrag){
+      for (var i = 0; i < this.bodies.length; i++) {
+        this.bodies[i].position.copy(this.meshes[i].position);
+        this.bodies[i].quaternion.copy(this.meshes[i].quaternion);
+      }
+    }
     for (var i = 0; i < this.meshes.length; i++) {
       this.meshes[i].position.copy(this.bodies[i].position);
       this.meshes[i].quaternion.copy(this.bodies[i].quaternion);
     }
+    
+    
+    
+    
 
     this.renderer.render(this.scene, this.camera);
   }
@@ -86,20 +129,20 @@ export class PendulumService {
     this.renderer.setSize(window.innerWidth, window.innerHeight);
   }
 
-  
   initCannon() {
     this.world = new CANNON.World();
     this.world.gravity.set(0, -10, 0);
     //this.world.broadphase=new CANNON.NaiveBroadphase();
 
     //ground material
-    //this.groundMaterial = new CANNON.Material();
+    this.groundMaterial = new CANNON.Material();
 
     //ball material
-    //this.physicMaterial = new CANNON.Material();
+    this.physicMaterial = new CANNON.Material();
 
-    //var ball_ground = new CANNON.ContactMaterial(this.groundMaterial, this.physicMaterial, { friction: 0.3, restitution: 0.3 });
-    //this.world.addContactMaterial(ball_ground);
+    var ball_ground = new CANNON.ContactMaterial(this.groundMaterial, this.physicMaterial, { friction: 0.3, restitution: 0.3 });
+    this.world.addContactMaterial(ball_ground);
+
   }
 
   createGround() {
@@ -108,238 +151,250 @@ export class PendulumService {
     floorGeo.applyMatrix(new THREE.Matrix4().makeRotationX(-Math.PI / 2));
 
     let material = new THREE.MeshStandardMaterial();
-    material.color= new THREE.Color(0xBA4859);
+    material.color = new THREE.Color(0xBA4859);
     //material.transparent= true;
 
-    let mesh = new THREE.Mesh(floorGeo, material);
-    this.meshes.push(mesh);
-    this.scene.add(mesh);
+    this.mesh = new THREE.Mesh(floorGeo, material);
+    this.scene.add(this.mesh);
 
     //Physics
     let groundShape = new CANNON.Plane();
-    let groundBody = new CANNON.Body({ mass: 0 });
+    let groundBody = new CANNON.Body({ mass: 0, material: this.groundMaterial });
     groundBody.addShape(groundShape);
     groundBody.quaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), -Math.PI / 2);
     this.world.addBody(groundBody);
-    this.bodies.push(groundBody);
   }
 
-  createChain(){
-    let size: .1;
-    let cylinderShape = new CANNON.Cylinder(size,size,size*2,8);
-    var quat = new CANNON.Quaternion(0.5,0,0,0.5);
-    quat.normalize();
+  addSphereChain() {
+    let size = 0.5;
+    let sphereShape = new CANNON.Box(new CANNON.Vec3(size / 2, size, size / 2));
     let mass = 0;
-    let last = null;
+    let space = 0.15 * size;
+    let lastBody = null;
     let N = 10;
+
     for (let i = 0; i < N; i++) {
-      const cylinderBody = new CANNON.Body({mass: i===0? mass:.5});
-      cylinderBody.addShape(cylinderShape,new CANNON.Vec3,quat);
-      cylinderBody.position.set(0,2-(i*size*2),0);
-      this.bodies.push(cylinderBody);
-      this.world.addBody(cylinderBody);
+      // Create a new body
+      let spherebody = new CANNON.Body({ mass: i === 0 ? mass : 0.5 });
+      spherebody.addShape(sphereShape);
+      spherebody.position.set(0, (N - i) * 2 * (size + space), 0);
+      this.bodies.push(spherebody);
+      this.world.addBody(spherebody);
+      //demo.addVisual(spherebody);
 
-      let cylinderGeo = new THREE.CylinderBufferGeometry(size,size,size*2,8);
-      let material = new THREE.MeshBasicMaterial({color:0x00ff00});
-      let cylinderMesh = new THREE.Mesh(cylinderGeo, material);
-      this.meshes.push(cylinderMesh);
-      this.scene.add(cylinderMesh);
-
-      // if(last){
-      //   const c = new CANNON.LockConstraint(cylinderMesh,last);
-      //   this.world.addContraint(c);
+      let sphereGeo = new THREE.BoxGeometry(size, size * 2, size, 10, 10);
+      let material = new THREE.MeshBasicMaterial({ color: 0xababab });
+      let sphereMesh = new THREE.Mesh(sphereGeo, material);
+      this.meshes.push(sphereMesh);
+      this.scene.add(sphereMesh);
+      // Connect this body to the last one added
+      // if(i!=0){
+      //     // Connect the current body to the last one
+      //     // We connect two corner points to each other.
+      //     var c1 = new CANNON.PointToPointConstraint(spherebody,new CANNON.Vec3(-size,size+space,0),lastBody,new CANNON.Vec3(-size,-size-space,0));
+      //     var c2 = new CANNON.PointToPointConstraint(spherebody,new CANNON.Vec3(size,size+space,0),lastBody,new CANNON.Vec3(size,-size-space,0));
+      //     this.world.addConstraint(c1);
+      //     this.world.addConstraint(c2);
+      // } else {
+      //     // First body is now static. The rest should be dynamic.
+      //     mass=0.3;
       // }
-      // last = cylinderBody;
-      
-    }
+      if (lastBody) {
+        // Connect the current body to the last one
+        var c = new CANNON.LockConstraint(spherebody, lastBody);
+        this.world.addConstraint(c);
+      }
 
+      // Keep track of the lastly added body
+      lastBody = spherebody;
+    }
   }
+
+
 }
 
 //DragControl function threejs
 function DragControls(_objects, _camera, _domElement) {
 
   if (_objects instanceof THREE.Camera) {
-     console.warn('THREE.DragControls: Constructor now expects ( objects, camera, domElement )');
+    console.warn('THREE.DragControls: Constructor now expects ( objects, camera, domElement )');
     var temp = _objects;
     _objects = _camera;
     _camera = temp;
-   }
-   var _plane = new THREE.Plane();
+  }
+  var _plane = new THREE.Plane();
   var _raycaster = new THREE.Raycaster();
-   var _mouse = new THREE.Vector2();
+  var _mouse = new THREE.Vector2();
   var _offset = new THREE.Vector3();
   var _intersection = new THREE.Vector3();
-   var _selected = null,
+  var _selected = null,
     _hovered = null;
-   //
-   var scope = this;
-   function activate() {
-     _domElement.addEventListener('mousemove', onDocumentMouseMove, false);
+  //
+  var scope = this;
+  function activate() {
+    _domElement.addEventListener('mousemove', onDocumentMouseMove, false);
     _domElement.addEventListener('mousedown', onDocumentMouseDown, false);
     _domElement.addEventListener('mouseup', onDocumentMouseCancel, false);
     _domElement.addEventListener('mouseleave', onDocumentMouseCancel, false);
     _domElement.addEventListener('touchmove', onDocumentTouchMove, false);
     _domElement.addEventListener('touchstart', onDocumentTouchStart, false);
     _domElement.addEventListener('touchend', onDocumentTouchEnd, false);
-   }
-   function deactivate() {
-     _domElement.removeEventListener('mousemove', onDocumentMouseMove, false);
+  }
+  function deactivate() {
+    _domElement.removeEventListener('mousemove', onDocumentMouseMove, false);
     _domElement.removeEventListener('mousedown', onDocumentMouseDown, false);
     _domElement.removeEventListener('mouseup', onDocumentMouseCancel, false);
     _domElement.removeEventListener('mouseleave', onDocumentMouseCancel, false);
     _domElement.removeEventListener('touchmove', onDocumentTouchMove, false);
     _domElement.removeEventListener('touchstart', onDocumentTouchStart, false);
     _domElement.removeEventListener('touchend', onDocumentTouchEnd, false);
-   }
-   function dispose() {
-     deactivate();
-   }
-   function onDocumentMouseMove(event) {
-     event.preventDefault();
-     var rect = _domElement.getBoundingClientRect();
-     _mouse.x = ((event.clientX - rect.left) / rect.width) * 2 - 1;
+  }
+  function dispose() {
+    deactivate();
+  }
+  function onDocumentMouseMove(event) {
+    event.preventDefault();
+    var rect = _domElement.getBoundingClientRect();
+    _mouse.x = ((event.clientX - rect.left) / rect.width) * 2 - 1;
     _mouse.y = -((event.clientY - rect.top) / rect.height) * 2 + 1;
-     _raycaster.setFromCamera(_mouse, _camera);
-     if (_selected && scope.enabled) {
-       if (_raycaster.ray.intersectPlane(_plane, _intersection)) {
-         _selected.position.copy(_intersection.sub(_offset));
-       }
-       scope.dispatchEvent({
+    _raycaster.setFromCamera(_mouse, _camera);
+    if (_selected && scope.enabled) {
+      if (_raycaster.ray.intersectPlane(_plane, _intersection)) {
+        _selected.position.copy(_intersection.sub(_offset));
+      }
+      scope.dispatchEvent({
         type: 'drag',
         object: _selected
       });
-       return;
-     }
-     _raycaster.setFromCamera(_mouse, _camera);
-     var intersects = _raycaster.intersectObjects(_objects);
-     if (intersects.length > 0) {
-       var object = intersects[0].object;
-       _plane.setFromNormalAndCoplanarPoint(_camera.getWorldDirection(_plane.normal), object.position);
-       if (_hovered !== object) {
-         scope.dispatchEvent({
+      return;
+    }
+    _raycaster.setFromCamera(_mouse, _camera);
+    var intersects = _raycaster.intersectObjects(_objects);
+    if (intersects.length > 0) {
+      var object = intersects[0].object;
+      _plane.setFromNormalAndCoplanarPoint(_camera.getWorldDirection(_plane.normal), object.position);
+      if (_hovered !== object) {
+        scope.dispatchEvent({
           type: 'hoveron',
           object: object
         });
-         _domElement.style.cursor = 'pointer';
+        _domElement.style.cursor = 'pointer';
         _hovered = object;
-       }
-     } else {
-       if (_hovered !== null) {
-         scope.dispatchEvent({
+      }
+    } else {
+      if (_hovered !== null) {
+        scope.dispatchEvent({
           type: 'hoveroff',
           object: _hovered
         });
-         _domElement.style.cursor = 'auto';
+        _domElement.style.cursor = 'auto';
         _hovered = null;
-       }
-     }
-   }
-   function onDocumentMouseDown(event) {
-     event.preventDefault();
-     _raycaster.setFromCamera(_mouse, _camera);
-     var intersects = _raycaster.intersectObjects(_objects);
-     if (intersects.length > 0) {
-       _selected = intersects[0].object;
-       if (_raycaster.ray.intersectPlane(_plane, _intersection)) {
-         _offset.copy(_intersection).sub(_selected.position);
-       }
-       _domElement.style.cursor = 'move';
-       scope.dispatchEvent({
+      }
+    }
+  }
+  function onDocumentMouseDown(event) {
+    event.preventDefault();
+    _raycaster.setFromCamera(_mouse, _camera);
+    var intersects = _raycaster.intersectObjects(_objects);
+    if (intersects.length > 0) {
+      _selected = intersects[0].object;
+      if (_raycaster.ray.intersectPlane(_plane, _intersection)) {
+        _offset.copy(_intersection).sub(_selected.position);
+      }
+      _domElement.style.cursor = 'move';
+      scope.dispatchEvent({
         type: 'dragstart',
         object: _selected
       });
-     }
+    }
 
   }
-   function onDocumentMouseCancel(event) {
-     event.preventDefault();
-     if (_selected) {
-       scope.dispatchEvent({
+  function onDocumentMouseCancel(event) {
+    event.preventDefault();
+    if (_selected) {
+      scope.dispatchEvent({
         type: 'dragend',
         object: _selected
       });
-       _selected = null;
-     }
-     _domElement.style.cursor = 'auto';
-   }
-   function onDocumentTouchMove(event) {
-     event.preventDefault();
+      _selected = null;
+    }
+    _domElement.style.cursor = 'auto';
+  }
+  function onDocumentTouchMove(event) {
+    event.preventDefault();
     event = event.changedTouches[0];
-     var rect = _domElement.getBoundingClientRect();
-     _mouse.x = ((event.clientX - rect.left) / rect.width) * 2 - 1;
+    var rect = _domElement.getBoundingClientRect();
+    _mouse.x = ((event.clientX - rect.left) / rect.width) * 2 - 1;
     _mouse.y = -((event.clientY - rect.top) / rect.height) * 2 + 1;
-     _raycaster.setFromCamera(_mouse, _camera);
-     if (_selected && scope.enabled) {
-       if (_raycaster.ray.intersectPlane(_plane, _intersection)) {
-         _selected.position.copy(_intersection.sub(_offset));
-       }
-       scope.dispatchEvent({
+    _raycaster.setFromCamera(_mouse, _camera);
+    if (_selected && scope.enabled) {
+      if (_raycaster.ray.intersectPlane(_plane, _intersection)) {
+        _selected.position.copy(_intersection.sub(_offset));
+      }
+      scope.dispatchEvent({
         type: 'drag',
         object: _selected
       });
-       return;
-     }
-   }
-   function onDocumentTouchStart(event) {
-     event.preventDefault();
+      return;
+    }
+  }
+  function onDocumentTouchStart(event) {
+    event.preventDefault();
     event = event.changedTouches[0];
-     var rect = _domElement.getBoundingClientRect();
-     _mouse.x = ((event.clientX - rect.left) / rect.width) * 2 - 1;
+    var rect = _domElement.getBoundingClientRect();
+    _mouse.x = ((event.clientX - rect.left) / rect.width) * 2 - 1;
     _mouse.y = -((event.clientY - rect.top) / rect.height) * 2 + 1;
-     _raycaster.setFromCamera(_mouse, _camera);
-     var intersects = _raycaster.intersectObjects(_objects);
-     if (intersects.length > 0) {
-       _selected = intersects[0].object;
-       _plane.setFromNormalAndCoplanarPoint(_camera.getWorldDirection(_plane.normal), _selected.position);
-       if (_raycaster.ray.intersectPlane(_plane, _intersection)) {
-         _offset.copy(_intersection).sub(_selected.position);
-       }
-       _domElement.style.cursor = 'move';
-       scope.dispatchEvent({
+    _raycaster.setFromCamera(_mouse, _camera);
+    var intersects = _raycaster.intersectObjects(_objects);
+    if (intersects.length > 0) {
+      _selected = intersects[0].object;
+      _plane.setFromNormalAndCoplanarPoint(_camera.getWorldDirection(_plane.normal), _selected.position);
+      if (_raycaster.ray.intersectPlane(_plane, _intersection)) {
+        _offset.copy(_intersection).sub(_selected.position);
+      }
+      _domElement.style.cursor = 'move';
+      scope.dispatchEvent({
         type: 'dragstart',
         object: _selected
       });
-     }
+    }
 
   }
-   function onDocumentTouchEnd(event) {
-     event.preventDefault();
-     if (_selected) {
-       scope.dispatchEvent({
+  function onDocumentTouchEnd(event) {
+    event.preventDefault();
+    if (_selected) {
+      scope.dispatchEvent({
         type: 'dragend',
         object: _selected
       });
-       _selected = null;
-     }
-     _domElement.style.cursor = 'auto';
-   }
-   activate();
-   // API
-   this.enabled = true;
-   this.activate = activate;
+      _selected = null;
+    }
+    _domElement.style.cursor = 'auto';
+  }
+  activate();
+  // API
+  this.enabled = true;
+  this.activate = activate;
   this.deactivate = deactivate;
   this.dispose = dispose;
-   // Backward compatibility
-   this.setObjects = function() {
-     console.error('THREE.DragControls: setObjects() has been removed.');
-   };
-   this.on = function(type, listener) {
-     console.warn('THREE.DragControls: on() has been deprecated. Use addEventListener() instead.');
+  // Backward compatibility
+  this.setObjects = function () {
+    console.error('THREE.DragControls: setObjects() has been removed.');
+  };
+  this.on = function (type, listener) {
+    console.warn('THREE.DragControls: on() has been deprecated. Use addEventListener() instead.');
     scope.addEventListener(type, listener);
-   };
-   this.off = function(type, listener) {
-     console.warn('THREE.DragControls: off() has been deprecated. Use removeEventListener() instead.');
+  };
+  this.off = function (type, listener) {
+    console.warn('THREE.DragControls: off() has been deprecated. Use removeEventListener() instead.');
     scope.removeEventListener(type, listener);
-   };
-   this.notify = function(type) {
-     console.error('THREE.DragControls: notify() has been deprecated. Use dispatchEvent() instead.');
+  };
+  this.notify = function (type) {
+    console.error('THREE.DragControls: notify() has been deprecated. Use dispatchEvent() instead.');
     scope.dispatchEvent({
       type: type
     });
-   };
- }
- DragControls.prototype = Object.create(THREE.EventDispatcher.prototype);
+  };
+}
+DragControls.prototype = Object.create(THREE.EventDispatcher.prototype);
 DragControls.prototype.constructor = DragControls;
-
-
